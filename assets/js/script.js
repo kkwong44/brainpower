80fe2582e8f6dbaaed4707ef3e0bf9cd0ba4e40f
kkwong44 <wong.k@sky.com>
2021-12-28 20:51:52 +0000

    feat: Add basic buttons event listener

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "new-game") {
+                alert("New Game");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`Undefine - ${gameType}`);
+            }
+        })
+    }
+})
