c3aa03cac7fd73f3842e21d8581f13a033c98675
kkwong44 <wong.k@sky.com>
2021-12-30 03:10:56 +0000

    maint: Add global const and var to functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,25 @@
+// Set maximum and minimum number of digits for the game
+const maxDigit = 8;
+const minDigit = 4;
+let numDigits = minDigit;
+let currentLevel = 1;
+
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 document.addEventListener("DOMContentLoaded", function () {
     // Create boxes to hold numbers for computer generated and player's answer
-    createNumberSquares("memory-box");
-    createNumberSquares("answer-box");
+    createNumberSquares("memory-box", maxDigit);
+    createNumberSquares("answer-box", maxDigit);
 
     // Reset game to initial state
-    resetGame();
+    resetGame(maxDigit);
 
     // Button event
     let buttons = document.getElementsByTagName("button");
     for (let button of buttons) {
         button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "new-game") {
-                runNewGame();
+                runNewGame(minDigit);
             } else {
                 let gameType = this.getAttribute("data-type");
                 alert(`Undefine - ${gameType}`);
@@ -26,12 +32,11 @@ document.addEventListener("DOMContentLoaded", function () {
  * Create squares to hold numbers
  * Use for random generated numbers and player's answer
  */
-function createNumberSquares(type) {
+function createNumberSquares(type, maxDigit) {
     let squares = "";
-    let numberOfDigit = 8;
     // check ID
     if (type == "memory-box" || type == "answer-box") {
-        for (let i = 1; i <= numberOfDigit; i++) {
+        for (let i = 1; i <= maxDigit; i++) {
             // Create html string for each memory square
             if (type == "memory-box") {
                 let square = `
@@ -95,9 +100,9 @@ function btnOverOut(id) {
 /**
  * Generate random numbers between 0 and 9
  */
-function randomGenerator(digits) {
+function randomGenerator(currentNumDigits) {
     let num = [];
-    for (let i = 0; i < digits; i++) {
+    for (let i = 0; i < currentNumDigits; i++) {
         let num1 = Math.floor(Math.random() * 10);
         num[i] = num1;
     }
@@ -107,10 +112,10 @@ function randomGenerator(digits) {
 /**
  * Display random numbers in the memory box
  */
-function displayNumbers(digits) {
-    randomGenerator(digits);
-    for (let i = 0; i < 8; i++) {
-        if (i < digits) {
+function displayNumbers(currentNumDigits) {
+    randomGenerator(currentNumDigits);
+    for (let i = 0; i < maxDigit; i++) {
+        if (i < currentNumDigits) {
             // Reset style and display Numbers in Memory Squares
             let num1 = Math.floor(Math.random() * 10);
             document.getElementsByClassName("memory-square")[i].children[0].innerHTML = num1;
@@ -130,9 +135,9 @@ function displayNumbers(digits) {
 /**
  * Reset game to initial state
  */
-function resetGame() {
+function resetGame(maxDigit) {
     // Empty and gray out all squares
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < maxDigit; i++) {
         document.getElementsByClassName("memory-square")[i].children[0].innerHTML = "";
         document.getElementsByClassName("answer-square")[i].value = "";
         document.getElementsByClassName("memory-square")[i].style.background = "#b9b9b9";
@@ -145,24 +150,24 @@ function resetGame() {
 }
 
 /**
- * Start a new game with 4 digits
+ * Start a new game with minmum number of digits
  */
- function runNewGame() {
-    displayNumbers(4);
+function runNewGame(currentNumDigits) {
+    displayNumbers(currentNumDigits);
     document.getElementById("levels").innerHTML = "1 of 20";
     btnDisabled("new-game", true);
     btnDisabled("submit", true);
     btnDisabled("next", true);
     // Hide numbers after 3 seconds
-    const  time = setTimeout(hideNumbers, 3000, 4);
+    const  time = setTimeout(hideNumbers, 3000, currentNumDigits);
     // timer();
 }
 
 /**
  * Hide random generated numbers, enable answer squares and submit button
  */
-function hideNumbers(digits) {
-    for (let i = 0; i < digits; i++) {
+function hideNumbers(currentNumDigits) {
+    for (let i = 0; i < currentNumDigits; i++) {
         document.getElementsByClassName("memory-square")[i].children[0].style.color = "#b9b9b9";
         document.getElementsByClassName("memory-square")[i].style.background = "#b9b9b9";
         document.getElementsByClassName("answer-square")[i].style.background = "white";
