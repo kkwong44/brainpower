f72e4d3123e0e0c60017f089c11c41e9686ee424
kkwong44 <wong.k@sky.com>
2022-01-10 22:10:08 +0000

    maint: Store constant values in session storage

--- assets/js/script-index.js
+++ assets/js/script-index.js
@@ -1,26 +1,46 @@
 // Script for index.html
 // Wait for the DOM to finish loading before running the game
 document.addEventListener("DOMContentLoaded", function () {
-
+    // Constants for the entire game
+    // The game can be changed from here. eg number of levels for the whole game
+    // Note: Number of digits beyond 8 will affect the display
+    const maxDigit = 8; // Maximum number of digits to remember
+    const minDigit = 4; // Minumum number of digits to remember
+    const maxLevel = 20; // Maximum number of levels
+    const gameInterval = 4; // Increase difficulties for every intererval on levels
+    const initialMemoryTime = 2000; // Initial memory time in milliseconds
+    const gameTimeInMinute = 10; // Maximum game time in minutes
+    // Add constants to session storage
+    sessionStorage.setItem("maxDigit", maxDigit);
+    sessionStorage.setItem("minDigit", minDigit);
+    sessionStorage.setItem("maxLevel", maxLevel);
+    sessionStorage.setItem("gameInterval", gameInterval);
+    sessionStorage.setItem("initialMemoryTime", initialMemoryTime);
+    sessionStorage.setItem("gameTimeInMinute", gameTimeInMinute);
+    // Load lists to page
     manualLists()
-
 })
 
 /**
  * Create manual lists for the home page 
  */
 function manualLists() {
+    // Get constant values from session storage
+    const minDigit = sessionStorage.getItem("minDigit");
+    const maxLevel = sessionStorage.getItem("maxLevel");
+    const gameInterval = sessionStorage.getItem("gameInterval");
+    const gameTimeInMinute = sessionStorage.getItem("gameTimeInMinute");
     // List for the game description
     let descList = `
         <li>This game is designed to test your memory.</li>
         <li>A random number will be generated and show on the screen for a short period of time.</li>
         <li>You need to remember this number before it disappeared from the screen.</li>
-        <li>The game starts with 4 digits.</li>
-        <li>Difficulties will increase for every 4 levels by adding an extra digit.</li>
-        <li>Time to memorise the number will also increase for every 4 levels.</li>
-        <li>There are 20 levels and the game is over when you completed the last level.</li>
-        <li>Each game has a maximum of 10 minutes to play.</li>
-        <li>The game will time out after 10 minutes and the score will not be recorded.</li>
+        <li>The game starts with ${minDigit} digits.</li>
+        <li>Difficulties will increase for every ${gameInterval} levels by adding an extra digit.</li>
+        <li>Time to memorise the number will also increase for every ${gameInterval} levels.</li>
+        <li>There are ${maxLevel} levels and the game is over when you completed the last level.</li>
+        <li>Each game has a maximum of ${gameTimeInMinute} minutes to play.</li>
+        <li>The game will time out after ${gameTimeInMinute} minutes and the score will not be recorded.</li>
     `;
     let list = document.getElementsByClassName("manual-list")[0];
     list.innerHTML = descList;
